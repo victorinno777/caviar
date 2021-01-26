1de2934c856328d2353e83189a02f5631f8fecc0
Victor <victorinno777@gmail.com@>
2021-01-26 17:55:11 +0200

    1

--- routes/index.js
+++ routes/index.js
@@ -84,7 +84,7 @@ router.post('/fetch', cors(corsOptions), async function (req, res) {
 	try {
 		const result = await getData();
 		//res.json(result);
-		res.send('dada');
+		res.send('dadad');
 	} catch (e) {
 		res.end(e.message || e.toString());
 	}
